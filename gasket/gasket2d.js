7286407c6434be49c472861e3db7fb6565124b06
Ubuntu <shd@masterseo01@naver.com>
2022-03-22 13:56:28 +0900

    gasket2d files[add]

--- gasket/gasket2d.js
+++ gasket/gasket2d.js
@@ -1,6 +1,6 @@
 var gl;
 var points;
-var numPoints = 5000;
+var numTimes = 5;
 
 window.onload = function init()
 {
@@ -10,13 +10,22 @@ window.onload = function init()
     if( !gl ) {
         alert("WebGL isn't available!");
     }
+    document.getElementById("level").onchange=function(event){
+        numTimes = event.target.value;
+
+        generateTriangles();
+
+        gl.bufferData(gl.ARRAY_BUFFER,flatten(points),gl.STATIC_DRAW);
+
+        render();
+    }
 
     // Sierpinski Gasket
-    generatePoints();
+    generateTriangles();
 
     // Configure WebGL
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -37,10 +46,10 @@ window.onload = function init()
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, points.length);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length);
 }
 
-function generatePoints() {
+function generateTriangles() {
     // Initialize the data for the Sierpinski Gasket
     // First, initialize the corners of a gasket with three points
     var vertices = [
@@ -48,34 +57,23 @@ function generatePoints() {
         vec2(0, 1),
         vec2(1, -1)
     ];
-
-    // Specify a starting point p for iterations
-    // p must lie inside any set of three vertices
-    var u = add(vertices[0], vertices[1]);
-    var v = add(vertices[0], vertices[2]);
-    var p = scale(0.25, add(u, v));
-
-    // Add an initial point into the array of points
-    points = [p];
-
+    
+    points=[];
     // Compute the new points
     // Each new point is located midway between last point and a randomly chosen vertex
-    for (var i=0; points.length<numPoints; i++) {
-        var j = Math.floor(Math.random() * 3);
-        p = add(points[i], vertices[j]);
-        p = scale(0.5, p);
-        points.push(p);
-    }
+    divideTriangle(vertices[0],vertices[1],vertices[2],numTimes);
 }
-function drawGasket(){
-    numPoints = parseInt(document.getElementById("numPoints").value);
-
-    if(numPoints>0&&numPoints<=50000){
-        generatePoints();
-        gl.bufferData(gl.ARRAY_BUFFER,flatten(points),gl.STATIC_DRAW);
-        render();
-    }
+function divideTriangle(a,b,c,count){
+    if(count==0)points.push(a,b,c);
     else{
-        alert("점의 개수는 0보다 크고 50,000보다 작거나 같아야 합니다.")
+        var ab=mix(a,b,0.5);
+        var bc=mix(b,c,0.5);
+        var ca=mix(c,a,0.5);
+
+        count--;
+
+        divideTriangle(a,ab,ca,count);
+        divideTriangle(b,bc,ab,count);
+        divideTriangle(c,ca,bc,count);
     }
 }
